335304bca948b1b1e5e80fa5a4c5e1782ce82f0e
Cristian Ignat <ignatcristian93@gmail.com>
2021-11-23 17:09:59 +0200

    Add controller and index.html

--- pet-clinic-web/src/main/java/com/ignat/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/ignat/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.ignat.sfgpetclinic.model;
+package com.ignat.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
