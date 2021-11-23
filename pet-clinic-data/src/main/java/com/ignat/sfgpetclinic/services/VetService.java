30109d3b4807318bc470e0faf6d214013865c40a
Cristian Ignat <ignatcristian93@gmail.com>
2021-11-23 16:21:22 +0200

    Create common CrudService

--- pet-clinic-data/src/main/java/com/ignat/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ignat/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.ignat.sfgpetclinic.services;
 
-import com.ignat.sfgpetclinic.model.Pet;
 import com.ignat.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Pet pet);
-
-    Set<Vet> findAll();
 }
